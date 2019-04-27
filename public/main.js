e877b1b7e93dca1ae411e987a0f3696b3cf7000a
Charles Bethin <cbethin@stevens.edu>
2019-04-27 19:55:26 -0400

    Handle response parse

--- public/main.js
+++ public/main.js
@@ -1,5 +1,11 @@
 console.log("Connected.")
 
+function handleNewScores(data) {
+    if (!data) { return; }
+    if (!data.hasOwnProperty('scores')) { return; }
+
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Document ready.")
 
@@ -12,7 +18,11 @@ document.addEventListener("DOMContentLoaded", () => {
             type: "POST",
             url: 'https://dr.charlesbethin.com/api/getTopicScores',
             data: JSON.stringify({"text": textBox.value}),
-            contentType: 'application/json'
+            contentType: 'application/json',
+            success: (data, success, xhttp) => {
+                jsonData = $.parseJSON(data)
+                handleNewScores(jsonData)
+            }
         })
     });
 })
